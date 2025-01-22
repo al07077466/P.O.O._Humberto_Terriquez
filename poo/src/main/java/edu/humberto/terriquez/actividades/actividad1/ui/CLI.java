49264a84ff568a13cc3e2945175404b0a310ab2d
Humberto Terriquez <al07077466@tecmilenio.mx>
2025-01-22 09:14:19 -0600

    first commit

---
+++ poo/src/main/java/edu/humberto/terriquez/actividades/actividad1/ui/CLI.java
@@ -0,0 +1,51 @@
+package edu.humberto.terriquez.actividades.actividad1.ui;
+
+import java.util.Scanner;
+
+public class CLI {
+    public static void runApp(){
+        Scanner scanner = new Scanner(System.in);
+        showMenu();
+        int opcion = scanner.nextInt();
+
+        while (opcion!=3){
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+            showMenu();
+            opcion = scanner.nextInt();
+        }
+    }
+
+    public static void showMenu() {
+        System.out.println("Menu" + 
+        "\n 1. Agregar Carro al Catalogo" + 
+        "\n 2. Mostrar Catalogo" + 
+        "\n 3. Salir");
+
+        //System.out.println("Menu");
+        //System.out.println("1. Agregar Carro al Catalogo");
+        //System.out.println("2. Mostrar Catalogo");
+        //System.out.println("3. Salir");
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public void mostrarError(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public void mostrarError(String[] estudiantes) {
+        for (String estudiante : estudiantes) {
+            System.out.println(estudiante);
+        }
+    }
+
+}
