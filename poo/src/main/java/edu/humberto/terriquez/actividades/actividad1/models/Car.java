bf0f895009306c1b1049f4ef13072e89e98f557b
Humberto Terriquez <al07077466@tecmilenio.mx>
2025-01-27 14:48:23 -0600

    Code running & Documentary finished, PDF's missing.

--- poo/src/main/java/edu/humberto/terriquez/actividades/actividad1/models/Car.java
+++ poo/src/main/java/edu/humberto/terriquez/actividades/actividad1/models/Car.java
@@ -1,5 +1,9 @@
 package edu.humberto.terriquez.actividades.actividad1.models;
 
+/**
+ * Declaracion de la clase "Car", con atrivutos privados String & Int
+ * usados para almacenar la informacion sobre un carro. (More on next comments)
+ */
 public class Car {
     private String modelo;
     private String marca;
@@ -7,16 +11,24 @@ public class Car {
     private int numeroPuertas;
     private String placa;
 
+    /**
+    * Metodo toString que convierte el objeto Car a una cadena de texto,
+    * utilizado para imprimir los atributos del objeto de manera legible.
+    */
     public String toString() {
         return "Car{ " +
                 "modelo='" + modelo + '\'' +
                 ", marca='" + marca + '\'' +
                 ", color='" + color + '\'' +
-                ", numeroPuertas=" + numeroPuertas + '\'' +
-                ", Placa=" + placa + '\'' +
+                ", numeroPuertas='" + numeroPuertas + '\'' +
+                ", Placa='" + placa + '\'' +
                 '}';
     }
 
+    /**
+    * Constructor de la clase Car, utilizado para crear nuevos objetos de tipo Car
+    * Cuenta con modelo, marca, color, numeroPuertas y placa como parametros.
+    */
     public Car(String modelo, String marca, String color, int numeroPuertas, String placa) {
         this.modelo = modelo;
         this.marca = marca;
@@ -25,6 +37,11 @@ public class Car {
         this.placa = placa;
     }
 
+    /**
+    * Metodos get y set,
+    * donde los getter devuelven el valor de los atributos privados de la clase &
+    * setter permiten modificar los valores de los atributos privados.
+    */
     public String getModelo() {
         return modelo;
     }
@@ -53,6 +70,10 @@ public class Car {
         return numeroPuertas;
     }
 
+    /**
+    * Utilizamos la excepcion "IllegalArgumentException" como validación
+    * para asegurarse de que el número de puertas esté entre 3 y 5.
+    */
     public void setNumeroPuertas(int numeroPuertas) {
         if(numeroPuertas<3 || numeroPuertas>5){
             throw new IllegalArgumentException("El Numero de Puertas Valido es de 3 a 5 (Incluye Hatchback)");
--- poo/src/main/java/edu/humberto/terriquez/actividades/actividad1/ui/CLI.java
+++ poo/src/main/java/edu/humberto/terriquez/actividades/actividad1/ui/CLI.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 import edu.humberto.terriquez.actividades.actividad1.process.StockManager;
 
+/**
+* CLI (Command Line Interface) es la interfaz creada para gestionar el menu
+* encargado de la gestion del stock de carros, este nos permite agregar carros y visualizar el stock.
+*/
 public class CLI {
+    /**
+    * "runApp" es el metodo corazon del programa, el cual se encarga de ejecutar y gestionar
+    * las 3 opciones disponibles en el menu y leer la opcion que el usario elija.
+    */
     public static void runApp(){
         Scanner scanner = new Scanner(System.in);
         StockManager stockManager = new StockManager();
@@ -13,6 +21,7 @@ public class CLI {
 
         while (opcion!=3){
             switch (opcion){
+                //Where case 1 agrega un carro al inventario.
                 case 1:
                     System.out.println("Introduce el Modelo");
                     String modelo = scanner.nextLine();
@@ -29,10 +38,12 @@ public class CLI {
                     stockManager.addCar(modelo, marca, color, numeroPuertas, placa);
                     System.out.println("Carro agregado con exito");
                     break;
+                //Where case 2 hace un display del stock actual.
                 case 2:
                     System.out.println("Stock Actual:");
                     stockManager.getStock().forEach(System.out::println);
                     break;
+                //Where default muestra un mensaje de error cuando una opcion no valida es elegida.
                 default:
                     System.out.println("Opcion no valida");
                     break;
@@ -41,30 +52,17 @@ public class CLI {
             opcion = scanner.nextInt();
             scanner.nextLine();
         }
+        //Y finaliza con la 3er opcion que muestra un mensaje de salida.
         System.out.println("Fin");
     }
 
+    /**
+    * "showMenu" es el metodo encargado de mostrar las opciones disponibles en la consola.
+    */
     public static void showMenu() {
         System.out.println("Menu" + 
         "\n 1. Agregar Carro al Catalogo" + 
         "\n 2. Mostrar Catalogo" + 
         "\n 3. Salir");
     }
-    
-    /* 
-    public void mostrarMensaje(String mensaje) {
-        System.out.println(mensaje);
-    }
-
-    public void mostrarError(String mensaje) {
-        System.out.println(mensaje);
-    }
-
-    public void mostrarError(String[] estudiantes) {
-        for (String estudiante : estudiantes) {
-            System.out.println(estudiante);
-        }
-    }
-    */
-
 }
